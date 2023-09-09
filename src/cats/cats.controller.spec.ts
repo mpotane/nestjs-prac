17d16e9fe8d522786b574e0d64c44a232fa56833
mpotane <65337802+mpotane@users.noreply.github.com>
2023-09-09 21:54:51 +0800

    update test

--- src/cats/cats.controller.spec.ts
+++ src/cats/cats.controller.spec.ts
@@ -1,18 +1,27 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CatsController } from './cats.controller';
+import { CatsService } from './cats.service';
 
 describe('CatsController', () => {
   let controller: CatsController;
+  let service: CatsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [CatsController],
+      providers: [CatsService],
     }).compile();
 
     controller = module.get<CatsController>(CatsController);
+    service = module.get<CatsService>(CatsService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  // service should be defined
+  it('should be defined', () => {
+    expect(service).toBeDefined();
   });
 });
--- src/cats/cats.service.ts
+++ src/cats/cats.service.ts
@@ -21,15 +21,10 @@ export class CatsService {
     return this.cats.slice(0, limit);
   }
 
-  findOne(id: number): Cat {
+  findOne(id: number): string | Cat {
     // find cat by id
     if (id > this.cats.length) {
-      return {
-        id: null,
-        name: 'Not found',
-        age: null,
-        breed: 'Not found',
-      };
+      return "Cat doesn't exist!";
     }
     return this.cats.find((cat) => cat.id === id);
   }
@@ -37,12 +32,7 @@ export class CatsService {
   update(id: number, cat: Cat) {
     const catIndex = this.cats.findIndex((cat) => cat.id === id);
     if (catIndex === -1) {
-      return {
-        id: null,
-        name: 'Not found',
-        age: null,
-        breed: 'Not found',
-      };
+      return "Cat doesn't exist!";
     }
     this.cats.splice(catIndex, 1, cat);
     return 'Cat has been updated!';
@@ -51,12 +41,7 @@ export class CatsService {
   deleteCat(id: number): string | Cat {
     const catIndex = this.cats.findIndex((cat) => cat.id === id);
     if (catIndex === -1) {
-      return {
-        id: null,
-        name: 'Not found',
-        age: null,
-        breed: 'Not found',
-      };
+      return "Cat doesn't exist!";
     }
     this.cats.splice(catIndex, 1);
     return 'Cat has been deleted!';
